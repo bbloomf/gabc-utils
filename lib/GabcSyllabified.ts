a37dcf27ea6c6483be5d6beae96e386ed028ebd6
Benjamin Bloomfield <bhb123@gmail.com>
2021-08-24 11:15:55 -0400

    fix(Easter Time directives): :bug: Fix handling of E.T. directive when displayed and the text ends with a melisma not attached to a syllable

--- lib/GabcSyllabified.ts
+++ lib/GabcSyllabified.ts
@@ -67,8 +67,8 @@ export class GabcSyllabified {
     } else {
       if (notationMatch && matchEasterTime) notation = notation.slice(0, notationMatch.index) + '::' + notationMatch[2];
       text = text.replace(
-        /([^,.;:\s])\s+\((E|T)\.\s*(T|P)\.\s*(a|A)([^)]+)\)([,.;:]*)/,
-        "$1$6 (<i>$2.$3.</i>) A$5$6"
+        /([^,.;:\s])\s+\((E|T)\.\s*(T|P)\.\s*(a|A)([^\s+-]*)([^)]+)\)([,.;:]*)/,
+        `$1$7 "<i>$2.$3.</i> A$5"$6$7`
       );
     }
     text = text
